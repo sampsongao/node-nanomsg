3196ea5f17b74ea58ceca419a6c6e04e3bfaf665
Sampson Gao <sampson.gao7@gmail.com>
2016-08-10 11:41:22 -0400

    Convert node_pointer.h to use NAPI

--- src/node_pointer.h
+++ src/node_pointer.h
@@ -3,31 +3,23 @@
 
 #pragma once
 
-#include <nan.h>
+#include <node_jsvmapi.h>
 
-/*
- * Called when the "pointer" is garbage collected.
- */
-
-// UNUSED: however, inline functions don't generate code.
-inline static void wrap_pointer_cb(char *data, void *hint) {
-  // fprintf(stderr, "wrap_pointer_cb\n");
-}
 
 /*
  * Wraps "ptr" into a new SlowBuffer instance with size "length".
  */
 
-inline static v8::Local<v8::Value> WrapPointer(void *ptr, size_t length) {
-  return Nan::NewBuffer(static_cast<char *>(ptr), length, wrap_pointer_cb, 0)
-      .ToLocalChecked();
+inline static napi_value WrapPointer(void *ptr, size_t length) {
+  napi_env env = napi_get_current_env();
+  return napi_buffer_copy(env, static_cast<char *>(ptr), length);
 }
 
 /*
  * Wraps "ptr" into a new SlowBuffer instance with length 0.
  */
 
-inline static v8::Local<v8::Value> WrapPointer(void *ptr) {
+inline static napi_value WrapPointer(void *ptr) {
   return WrapPointer(ptr, 0);
 }
 
@@ -35,9 +27,10 @@ inline static v8::Local<v8::Value> WrapPointer(void *ptr) {
  * Unwraps Buffer instance "buffer" to a C `char *` with the offset specified.
  */
 
-inline static char *UnwrapPointer(v8::Local<v8::Value> buffer, int64_t offset) {
-  if (node::Buffer::HasInstance(buffer)) {
-    return node::Buffer::Data(buffer.As<v8::Object>()) + offset;
+inline static char *UnwrapPointer(napi_value buffer, int64_t offset) {
+  napi_env env = napi_get_current_env();
+  if (napi_buffer_has_instance(env, buffer)) {
+    return napi_buffer_data(env, buffer) + offset;
   } else {
     return 0;
   }
@@ -47,9 +40,10 @@ inline static char *UnwrapPointer(v8::Local<v8::Value> buffer, int64_t offset) {
  * Unwraps Buffer instance "buffer" to a C `char *` (no offset applied).
  */
 
-inline static char *UnwrapPointer(v8::Local<v8::Value> buffer) {
-  if (node::Buffer::HasInstance(buffer)) {
-    return node::Buffer::Data(buffer.As<v8::Object>());
+inline static char *UnwrapPointer(napi_value buffer) {
+  napi_env env = napi_get_current_env();
+  if (napi_buffer_has_instance(env, buffer)) {
+    return napi_buffer_data(env, buffer);
   } else {
     return 0;
   }
@@ -61,6 +55,6 @@ inline static char *UnwrapPointer(v8::Local<v8::Value> buffer) {
  */
 
 template <typename Type>
-inline static Type UnwrapPointer(v8::Local<v8::Value> buffer) {
+inline static Type UnwrapPointer(napi_value buffer) {
   return reinterpret_cast<Type>(UnwrapPointer(buffer));
 }
