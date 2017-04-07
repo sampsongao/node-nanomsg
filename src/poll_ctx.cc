71aa5fb166f513def34fd4e310b119c9c956217f
Sampson Gao <sampsong@ca.ibm.com>
2017-04-07 15:05:18 -0400

    Port to node-api wrapper experimentally

--- src/poll_ctx.cc
+++ src/poll_ctx.cc
@@ -1,10 +1,9 @@
 #include "nn.h"
 #include "poll_ctx.h"
 
-using v8::Function;
-using v8::Local;
-using v8::Number;
-using v8::Value;
+using Napi::Function;
+using Napi::Number;
+using Napi::Value;
 
 static void NanomsgReadable(uv_poll_t* req, int /* status */, int events) {
   const PollCtx* const context = static_cast<PollCtx*>(req->data);
@@ -24,7 +23,7 @@ void PollCtx::begin_poll (const int s, const bool is_sender) {
 }
 
 PollCtx::PollCtx (const int s, const bool is_sender,
-    const Local<Function> cb): callback(cb) {
+    const Napi::Function cb): callback(cb) {
   // TODO: maybe container_of can be used instead?
   // that would save us this assignment, and ugly static_cast hacks.
   poll_handle.data = this;
@@ -32,22 +31,20 @@ PollCtx::PollCtx (const int s, const bool is_sender,
 }
 
 void PollCtx::invoke_callback (const int events) const {
-  Nan::HandleScope scope;
-  Local<Value> argv[] = { Nan::New<Number>(events) };
-  callback.Call(1, argv);
+  Napi::HandleScope scope(callback.Env());
+  callback.MakeCallback({ Napi::Number::New(callback.Env(), events) });
 }
 
 // Nan will invoke this once it's done with the Buffer, in case we wanted to
 // free ptr.  In this case, ptr is a PollCtx that we're not done with and don't
 // want to free yet (not until PollStop is invoked), so we do nothing.
-static void wrap_pointer_cb(char * /* data */, void * /* hint */) {}
+static void wrap_pointer_cb(napi_env env, void * /* data */, void * /* hint */) {}
 
-Local<Value> PollCtx::WrapPointer (void* ptr, size_t length) {
-   return Nan::NewBuffer(static_cast<char *>(ptr), length, wrap_pointer_cb, 0)
-     .ToLocalChecked();
+Napi::Value PollCtx::WrapPointer (Napi::Env env, void* ptr, size_t length) {
+   return Napi::Buffer<char>::New(env, static_cast<char *>(ptr), length, wrap_pointer_cb, 0);
 }
 
-PollCtx* PollCtx::UnwrapPointer (v8::Local<v8::Value> buffer) {
-  return reinterpret_cast<PollCtx*>(node::Buffer::HasInstance(buffer) ?
-    node::Buffer::Data(buffer.As<v8::Object>()) : NULL);
+PollCtx* PollCtx::UnwrapPointer (Napi::Value buffer) {
+  return reinterpret_cast<PollCtx*>(buffer.IsBuffer() ?
+    buffer.As<Napi::Buffer<char>>().Data() : NULL);
 }
